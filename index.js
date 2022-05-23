8cbd7176d4ee628229ec44f9c35b324b15888ef0
Al-amin Sheikh <azalamins@gmail.com>
2022-05-23 14:03:24 +0600

    added parts get api and single parts api

---
+++ index.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
+
+const port = process.env.PORT || 5000;
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dhgpv.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const userCollection = client.db("RinspeedCar").collection("user");
+    const partsCollection = client.db("RinspeedCar").collection("parts");
+    const orderCollection = client.db("RinspeedCar").collection("order");
+    console.log("db connected");
+
+    app.get("/parts", async (req, res) => {
+      const result = (
+        await partsCollection.find().limit(6).toArray()
+      ).reverse();
+      res.send(result);
+    });
+
+    app.get("/parts/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await partsCollection.findOne(query);
+      res.send(result);
+    });
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Welcome to Todo app");
+});
+
+app.listen(port, () => {
+  console.log("Listen to the port", port);
+});
