ed0203a6d0b40f2b3cb4b26869da89c5c3ee30f4
Al-amin Sheikh <azalamins@gmail.com>
2022-05-25 00:45:11 +0600

    added jwt

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+const jwt = require("jsonwebtoken");
 
 const port = process.env.PORT || 5000;
 const app = express();
@@ -16,6 +17,33 @@ const client = new MongoClient(uri, {
   useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1,
 });
+
+const verifyJWT = (req, res, next) => {
+  const authHeader = req.headers?.authorization;
+  const token = authHeader?.split(" ")[1];
+  if (!authHeader) {
+    return res.status(401).send({ message: "UnAuthorized Access" });
+  }
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+    if (err) {
+      return res.status(403).send({ message: "Forbidden Access" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+};
+
+const verifyAdmin = async (req, res, next) => {
+  const requester = req.decoded.email;
+  const requesterAccount = await userCollection.findOne({
+    email: requester,
+  });
+  if (requesterAccount.role === "admin") {
+    next();
+  } else {
+    res.status(403).send({ message: "Forbidden access" });
+  }
+};
 
 async function run() {
   try {
@@ -55,29 +83,21 @@ async function run() {
         updatedUser,
         options
       );
-      //  const token = jwt.sign(
-      //    { email: email },
-      //    process.env.ACCESS_TOKEN_SECRET,
-      //    { expiresIn: "1h" }
-      //  );
-      res.send(result);
+       const token = jwt.sign(
+         { email: email },
+         process.env.ACCESS_TOKEN_SECRET,
+         { expiresIn: "1d" }
+       );
+      res.send({ result, accessToken: token });
     });
 
-    // Make Admin
-    app.patch("/user/:email", async (req, res) => {
+    app.put("/user/admin/:email", verifyJWT, verifyAdmin, async (req, res) => {
       const email = req.params?.email;
       const filter = { email: email };
-      const options = { upsert: true };
       const updatedUser = {
-        $set: {
-          role: "admin",
-        },
+        $set: { role: "admin" },
       };
-      const result = await userCollection.updateOne(
-        filter,
-        updatedUser,
-        options
-      );
+      const result = await userCollection.updateOne(filter, updatedUser);
       res.send(result);
     });
 
@@ -89,11 +109,17 @@ async function run() {
       res.send(result);
     });
 
+    // SIngle user api
+    app.get("/admin/:email", async (req, res) => {
+      const email = req.params.email;
+      const user = await userCollection.findOne({ email: email });
+      const isAdmin = user?.role === "admin";
+      res.send({ admin: isAdmin });
+    });
+
     // Get Users
     app.get("/user", async (req, res) => {
-      const result = (
-        await userCollection.find().toArray()
-      ).reverse();
+      const result = (await userCollection.find().toArray()).reverse();
       res.send(result);
     });
 
